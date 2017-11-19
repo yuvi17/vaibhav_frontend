24c5ff64717766a7d820d0777cfc1edb1facd6c7
Yuvraj Jaiswal <yjaiswal@yjaiswal-m01.vmware.com>
2017-11-19 21:20:32 +0530

    changes to ui

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angularjs-datetime-picker/angularjs-datetime-picker.js',
+      'bower_components/materialize/dist/js/materialize.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
